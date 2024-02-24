a2f8e0079dc9af84e60ba106e411d9d79abe3d65
Hassam Tariq <151511092+hassam567@users.noreply.github.com>
2024-02-24 18:17:36 +0500

    chnages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import RoutesArray from './routes/routesArray';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import CommentsTable from './TablesData/CommentsTable';
 import UsersTable from './TablesData/UsersTable';
-import { BrowserRouter as Router,Routes,Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Instructions from './Instructions';
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-  <QueryClientProvider client={queryClient}>
-   <BrowserRouter  basename='/react_table'>
-      <Routes>
-      <Route path="/react_table" element={<Instructions />} />
-        <Route path="/react_table/users" element={<UsersTable />} />
-        <Route path="/react_table/comments" element={<CommentsTable />} />
-       
-        {RoutesArray()}
-      </Routes>
-    </BrowserRouter>
-    </QueryClientProvider>
-   
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/react_table" element={<Instructions />} />
+            <Route path="/react_table/users" element={<UsersTable />} />
+            <Route path="/react_table/comments" element={<CommentsTable />} />
+            {RoutesArray()}
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </>
-   
   );
 }
 
